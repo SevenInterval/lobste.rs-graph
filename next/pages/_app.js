afaf20de25a596649cc05618d69eeb3bfbe3d992
SevenInterval <ridvanozturk94@gmail.com>
2023-08-23 13:42:19 +0300

    react-force-graph entegrasyon

--- next/pages/_app.js
+++ next/pages/_app.js
@@ -1,5 +1,25 @@
-import '@/styles/globals.css'
+import {
+  ApolloProvider,
+  ApolloClient,
+  InMemoryCache,
+  HttpLink
+} from "@apollo/client"
+
+const createApolloClient = () => {
+  const link = new HttpLink({
+    uri: '/api/graphql'
+  })
+
+  return new ApolloClient({
+    link,
+    cache: new InMemoryCache()
+  })
+}
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ApolloProvider client={createApolloClient()}>
+      <Component {...pageProps} />
+    </ApolloProvider>
+  );
 }
